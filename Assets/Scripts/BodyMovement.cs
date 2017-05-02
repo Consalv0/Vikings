0924bef84f4611244148ab40e928f13214c8006b
Consalvo <gonzaloceciliano@gmail.com>
2017-05-01 21:23:04 -0500

    Body Rotation

---
+++ Assets/Scripts/BodyMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 bodyPos = Camera.main.WorldToScreenPoint(transform.position);
+		Vector3 dir = Input.mousePosition - bodyPos;
+		float newAngle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg) + 90;
+		Quaternion newRotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
+		transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, 0.05f);
+	}
+}
